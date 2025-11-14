92e275d03566f398f17fe56e5ad8215ed41a2fc7
Victor <victorfoliveira0407@gmail.com>
2025-11-14 16:21:39 -0300

    feat: continua table

--- sql/DDL.sql
+++ sql/DDL.sql
@@ -1,5 +1,5 @@
 CREATE TABLE clientes (
-	cpf CHAR(14) PRIMARY KEY UNIQUE,
+	cpf CHAR(14) PRIMARY KEY,
 	nome VARCHAR(255) NOT NULL,
 	email VARCHAR(255) NOT NULL UNIQUE,
 	telefone CHAR(13) NOT NULL UNIQUE,
@@ -7,6 +7,31 @@ CREATE TABLE clientes (
 );
 
 CREATE TABLE categorias (
-	id SERIAL PRIMARY KEY,
-	categoria VARCHAR(20)
+	id_categoria SERIAL PRIMARY KEY,
+	categoria VARCHAR(20) NOT NULL
+);
+
+CREATE TABLE fornecedores (
+	id_fornecedor SERIAL PRIMARY KEY,
+	fornecedor VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE produtos (
+	id_produto SERIAL PRIMARY KEY,
+	nome_produto VARCHAR(255) NOT NULL,
+	id_categoria INT NOT NULL,
+	marca VARCHAR(50) NOT NULL,
+	preco DECIMAL(10,2) NOT NULL CHECK(preco >= 0),
+	estoque INT NOT NULL CHECK(estoque >= 0),
+	id_fornecedor INT not NULL,
+
+	CONSTRAINT fk_produtos_categorias
+	FOREIGN KEY (id_categoria) REFERENCES categorias(id_categoria),
+
+	CONSTRAINT fk_produtos_fornecedores
+	FOREIGN KEY (id_fornecedor) REFERENCES fornecedores(id_fornecedor)
+);
+
+CREATE TABLE vendas (
+	
 );
